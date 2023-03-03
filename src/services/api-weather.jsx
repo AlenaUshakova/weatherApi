b87c1b120f88a3fe346bf4ca2b439570582082d8
Alena Ushakova <110482834+AlenaUshakova@users.noreply.github.com>
2023-03-03 19:36:38 +0200

    2

--- src/services/api-weather.jsx
+++ src/services/api-weather.jsx
@@ -19,7 +19,7 @@ import axios from 'axios';
 // };
 
 export const weatherByCurr = async (query,day) => {
-  axios.defaults.baseURL = `http://api.weatherapi.com/v1/`;
+  axios.defaults.baseURL = `https://api.weatherapi.com/v1/`;
   const KEY2 = 'e8751ca91012454b968145713232702';
   const response = await axios.get(
     `forecast.json?key=${KEY2}&q=${query}&days=${day}`
